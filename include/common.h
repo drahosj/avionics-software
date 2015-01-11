0ec30a5d8c511b21fa0613364cae30ea1722c0bd
Jake Drahos <drahos@iastate.edu>
2015-01-11 17:56:30 -0600

    Added I2C and SPI from CySat

--- include/common.h
+++ include/common.h
@@ -2,6 +2,9 @@
 
 #include "stdint.h"
 
+#define pdTRUE 1
+#define pdFALSE 0
+
 
 typedef uint8_t status_t;
 #define STATUS_SUCCESS 0
@@ -9,3 +12,6 @@ typedef uint8_t status_t;
 #define STATUS_TXBUFFER_FULL 81
 #define STATUS_RINGBUFFER_FULL 82
 #define STATUS_RXBUFFER_EMPTY 83
+
+#define configKERNEL_INTERRUPT_PRIORITY 255
+#define configLIBRARY_KERNEL_INTERRUPT_PRIORITY 15
