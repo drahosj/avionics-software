bd6af09265dc016d3da5f4c158d6c890487bda93
Jake Drahos <drahos@iastate.edu>
2015-02-12 20:14:20 -0600

    Made it work on real hardware

--- src/main.c
+++ src/main.c
@@ -21,6 +21,8 @@ static void lowLevelHardwareInit()
 {
     NVIC_PriorityGroupConfig(NVIC_PriorityGroup_4);
     RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE);
+    RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOC, ENABLE);
+    RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOB, ENABLE);
     RCC_APB1PeriphClockCmd(RCC_APB1Periph_PWR, ENABLE);
 }
 
@@ -31,18 +33,38 @@ int main( void )
     /* Test Heartbeat Initialization */
     GPIO_InitTypeDef GPIO_InitStructure;
     
-    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_5;
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0 | GPIO_Pin_1 | GPIO_Pin_2 | GPIO_Pin_3;
     GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
     GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
     GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP;
     GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
     
-    GPIO_Init(GPIOA, &GPIO_InitStructure);
+    GPIO_Init(GPIOC, &GPIO_InitStructure);
+
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_12 | GPIO_Pin_13 | GPIO_Pin_14 | GPIO_Pin_15;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
+    GPIO_Init(GPIOB, &GPIO_InitStructure);
     /* End test heartbeat initialization */
 
-    startClock();
+//    startClock();
+
+    SysTick_Config(SystemCoreClock/2);
     
     for(;;)
     {
     }
+}
+
+static uint8_t value = 0;
+void SysTick_Handler()
+{
+    uint8_t tmp = 0;
+    tmp = GPIOC->ODR;
+    value++;
+    if (value > 0x03)
+	value = 0;
+
+    tmp &= 0xFFF0;
+    tmp |= ~((GPIOB->IDR & 0xF000) >> 12);
+    GPIOC->ODR = tmp;
 }
